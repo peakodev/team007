8643a1f5883fff4396f909159fab879f13efcb5e
Orest <lav.orest@gmail.com>
2024-02-20 19:58:31 +0200

    Supports all commands for Agent notes, adds, removes, display notes as long as it can import AgentNote

--- jamesbot/jamesbot/completer.py
+++ jamesbot/jamesbot/completer.py
@@ -1,16 +1,16 @@
-#from prompt_toolkit import prompt   #just for test can be removed later
+# from prompt_toolkit import prompt   #just for test can be removed later
 from prompt_toolkit.completion import NestedCompleter
 
 completer = NestedCompleter.from_nested_dict({
-    'show': {'all'},
-    'hello': None,
-    'add': None,
-    'change': None,
-    'phone': None,
-    'delete': None,
+    'show': {'notes': {'all': None}},
+    'note': {'add': None},
+    'add': {'note': None},
+    'edit': {'note': None},
+    'remove': {'note': None},
+    'find': {'notes': None},
     'help': None,
     'exit': None,
 })
 
-#text = prompt('# ', completer=completer) # #just for test can be removed later
-#print('You said: %s' % text)   #  #just for test can be removed later
+# text = prompt('# ', completer=completer) # #just for test can be removed later
+# print('You said: %s' % text)   #  #just for test can be removed later
--- jamesbot/jamesbot/james-notes.py
+++ jamesbot/jamesbot/james-notes.py
@@ -1,4 +1,5 @@
-# import agent_notes
+# from agent_notes import AgentNotes
+#from entities import AgentNotes
 from prompt_toolkit import prompt
 from toolbar import style, bottom_toolbar, rprompt
 from completer import completer
@@ -6,15 +7,14 @@ from completer import completer
 
 def show_help():
     available_commands = {
-        'hello': "say hello",
-        'add': "add contact, example add john 1234567890",
-        'change': 'change number in existing contact. example: change John old_phone new_phone',
-        'phone': "find contact by name. example: phone John",
-        'show all': "returns all contacts in book",
-        'delete': "remove contact from the book, example: delete john",
-        'birthday': "set a birthday for contact, example: birthday john 2022-11-11",
-        'bday': 'return days till next birthday of a contact, example: bday john',
-        'find': 'find any matches in contact name or phones, example: find xxx'
+        'add note': "add new note , you can specify tags with --",
+        'show notes all': "display all notes",
+        'note add tag': 'add a tag to existing note by id',
+        'edit note': 'edit existing note by id',
+        'remove note': "remove existing note by id",
+        'find notes': 'find notes by any matches in text or id',
+        'exit': "for exit",
+        'help': show_help
     }
     for key, description in available_commands.items():
         print("{:<10} -> {}".format(key, description))
@@ -47,8 +47,17 @@ class Bot:
     def running(self, value):
         self.__isRunning = value
 
+    @property
     def notes(self):  # deserialize notes
-        pass
+        if self.__notes is None:
+            self.__notes = AgentNotes().deserialize()
+        return self.__notes
+
+    # # @property
+    # def book(self) -> AgentBook:
+    #     if self.__book is None:
+    #         self.__book = AgentBook().deserialize()
+    #     return self.__book
 
     @property
     def commands(self) -> dict:
@@ -58,24 +67,19 @@ class Bot:
     def commands(self, commands: dict):
         self.__commands = commands
 
-    @property
-    def note(self):
-        pass
-
-    @note.setter
-    def note(self, id):
-        pass
-
 
 def bot_exit():
     print("Good bay")
+    Bot().notes.serialize
     Bot().running = False
 
 
 def input_handler(input_string):
-    separated = input_string.split()
-    for i in separated:
-        entered_command = ''.join(i)
+    # separated = input_string.split()
+    entered_command = ''
+    for i in input_string:
+        entered_command += ''.join(i)
+        print(entered_command)
         if entered_command in Bot().commands.keys():
             return entered_command, input_string[len(entered_command):]
     raise ValueError
@@ -83,31 +87,39 @@ def input_handler(input_string):
 
 def command_handler(command, input_string):
     user_command = Bot().commands.get(command)
-    user_command(*input_string)
+    if input_string:
+        text, *tag = input_string.strip().split('--')
+        ids = None
+        if text.strip().split(' ', 1)[0].isdigit():
+            ids, *text = text.strip().split(' ', 1)
+            user_command(ids, *text)
+            return
+        user_command(text, *tag)
+    else:
+        user_command()
 
 
 def bot_start():
     bot = Bot()
     COMMANDS = {
-        'add': bot.note,
+        'add note': bot.notes.add_note,
         'exit': bot_exit,
-        'change': None,
-        'phone': None,
-        'show all': None,
-        'wrong command': None,
-        'delete': None,
-        'birthday': None,
-        'bday': None,
-        'find': None,
+        'show notes all': bot.notes.show_all_notes,
+        'note add tag': bot.notes.add_note_tag,
+        'edit note': bot.notes.edit_note,
+        'remove note': bot.notes.remove_note,
+        'find notes': bot.notes.find_notes,
         'help': show_help
     }
     bot.commands = COMMANDS
     while bot.running:
-        user_input = prompt(">>",completer=completer, bottom_toolbar=bottom_toolbar, style=style, rprompt=rprompt)
-        print(user_input)
-        user_command, input_string = input_handler(str(user_input))
-        command_handler(user_command, input_string)
-
+        try:
+            user_input = prompt(">>", completer=completer, bottom_toolbar=bottom_toolbar, style=style, rprompt=rprompt)
+            user_command, input_string = input_handler(str(user_input))
+            print(len(input_string))
+            command_handler(user_command, input_string)
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     bot_start()
