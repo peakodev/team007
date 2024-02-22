ed402e2bc01a310e22dfe73ef907022f62d70142
Orest <lav.orest@gmail.com>
2024-02-22 12:51:38 +0200

    book show all

--- jamesbot/jamesbot/james-notes-yriy.py
+++ jamesbot/jamesbot/james-notes-yriy.py
@@ -1,15 +1,17 @@
 # from agent_book.agent_book.entities import AgentBook
-from agent_book import AgentBook
+from agent_book import AgentBook, AgentBookIterator
+from agent_notes import AgentNotes, format
 from prompt_toolkit import prompt
 from toolbar import style, bottom_toolbar, rprompt
 from completer2 import completer
 from colorama import init
-
+from bot_class import Bot
 from colorama import Fore, Back, Style
 
 init()
-book = AgentBook()
- 
+book = Bot().book
+
+
 def show_help():
     available_commands = {
         'hello': "say hello",
@@ -29,7 +31,6 @@ def show_help():
 
 
 def check_param(num_of_param, plus='+'):
-    
     def check(func):
         if num_of_param >= 10:
             beg = num_of_param // 10
@@ -37,25 +38,26 @@ def check_param(num_of_param, plus='+'):
         else:
             beg = num_of_param
             end = beg
-            
+
         def wrapper(*arg):
-            param  = tuple(filter(lambda x: x, ''.join(arg).split(' ')))
+            param = tuple(filter(lambda x: x, ''.join(arg).split(' ')))
             if len(param) != beg and len(param) != end:
                 raise ValueError('Error: Invalid number of parameters. ' + Fore.LIGHTBLUE_EX + \
-                    f'Expected {beg} received {len(param)}' + Fore.RESET) 
+                                 f'Expected {beg} received {len(param)}' + Fore.RESET)
 
             record = None
             if plus == '+':
                 record = book.find_record(param[0])
                 if record == None:
-                    raise ValueError('User '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RED+\
-                        ' is not in the phone book')
-                
+                    raise ValueError('User ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RED + \
+                                     ' is not in the phone book')
+
             return func(param, record)
-            
+
         return wrapper
-    
+
     return check
+
 
 @check_param(23, '-')
 def add_user(param, empty):
@@ -65,109 +67,140 @@ def add_user(param, empty):
         birthday = param[2].replace('.', '-')
     else:
         birthday = None
-        
+
     book.add(name_user, num_phone, birthday)
-    return 'User '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET+' added to phone book'
-    
+    return 'User ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET + ' added to phone book'
+
+
 @check_param(1, '-')
 def del_user(param, empty):
     book.delete(param[0])
-    return 'User '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET+\
-           ' has been removed from the phone book '
+    return 'User ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET + \
+        ' has been removed from the phone book '
 
-@check_param(3) 
+
+@check_param(3)
 def change_phone(param, record):
     record.edit_phone(param[1], param[2])
-    return 'User '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET+\
-           ' phone number '+Fore.LIGHTBLUE_EX+f' {param[1]} '+Fore.RESET+\
-            ' replaced with '+Fore.LIGHTBLUE_EX+f' {param[2]} '+Fore.RESET
-    
-    
+    return 'User ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET + \
+        ' phone number ' + Fore.LIGHTBLUE_EX + f' {param[1]} ' + Fore.RESET + \
+        ' replaced with ' + Fore.LIGHTBLUE_EX + f' {param[2]} ' + Fore.RESET
+
+
 @check_param(2)
 def add_phone(param, record):
     record.add_phone(param[1])
-    return f'Phone number '+Fore.LIGHTBLUE_EX+f' {param[1]} '+Fore.RESET+\
-        ' has been added to user '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET
+    return f'Phone number ' + Fore.LIGHTBLUE_EX + f' {param[1]} ' + Fore.RESET + \
+        ' has been added to user ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET
+
 
 @check_param(2)
 def add_birthday(param, record):
     record.add_birthday(param[1].replace('.', '-'))
-    return 'User '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET+\
-           ' has a birthday recorded '+Fore.LIGHTBLUE_EX+f' {param[1]} '+Fore.RESET
-    
+    return 'User ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET + \
+        ' has a birthday recorded ' + Fore.LIGHTBLUE_EX + f' {param[1]} ' + Fore.RESET
+
+
 @check_param(2)
 def del_phone(param, record):
     record.remove_phone(param[1])
-    return f'Phone number '+Fore.LIGHTBLUE_EX+f'{param[1]}'+Fore.RESET+\
-            ' has been deleted for user '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET
-    
-    
+    return f'Phone number ' + Fore.LIGHTBLUE_EX + f'{param[1]}' + Fore.RESET + \
+        ' has been deleted for user ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET
+
+
 @check_param(1)
 def find_phone(param, record):
-    phone = record.find_phone(param[0])        # Не понятна работа !!!!!!!!!!! 
+    phone = record.find_phone(param[0])  # Не понятна работа !!!!!!!!!!!
     return phone
+
 
 @check_param(1)
 def next_birthday(param, record):
     next_day = record.days_to_birthday()
-    return 'User '+Fore.LIGHTBLUE_EX+f' {param[0]} '+Fore.RESET+\
-        ' has a birthday in '+Fore.LIGHTBLUE_EX+f'{next_day} days'+Fore.RESET
+    return 'User ' + Fore.LIGHTBLUE_EX + f' {param[0]} ' + Fore.RESET + \
+        ' has a birthday in ' + Fore.LIGHTBLUE_EX + f'{next_day} days' + Fore.RESET
 
-@check_param(1,'-')
+
+@check_param(1, '-')
 def find_users(param, record):
     list_user = book.find(param[0])
     if len(list_user) == 0:
-       return Fore.LIGHTYELLOW_EX+'Users with the specified parameters were not found'
-        
+        return Fore.LIGHTYELLOW_EX + 'Users with the specified parameters were not found'
+
     for name_user in list_user:
         print(name_user)
-    
-
-def singleton(class_):
-    instances = {}
-
-    def getinstance(*args, **kwargs):
-        if class_ not in instances:
-            instances[class_] = class_(*args, **kwargs)
-        return instances[class_]
-
-    return getinstance
 
 
-@singleton
-class Bot:
-    def __init__(self):
-        self.__isRunning = True
-        self.__book = None
-        self.__notes = None
-        self.__commands = dict()
+def show_all():
+    max_key_length = max(len(key) for key in book.keys())
+    print(max_key_length)
 
-    @property
-    def running(self):
-        return self.__isRunning
+    print("  | {} | {} | {} | {} | {} |".format('Agent call sign'.ljust(max_key_length), 'Email'.ljust(max_key_length),
+                                                'Phones'.ljust(max_key_length), 'Birthday', 'Address'))
+    for value in book.data.items():
+        # print(value[1].call_sign)
+        # print(value[1].email)
+        Name = str(value[1].call_sign)
+        email = str(value[1].email)
+        phone = value[1].phones
+        # birthday =
+        # adress =
+        # for phone in value[1].phones:
+        #     print(phone)
+        # if value[1].birthday:
+        #     print(value[1].days_to_birthday())
+        # if value[1].address:
+        #     print(str(value[1].address))
+        print("  | {:11} | {:11} | {:11} |   |   |".format(Name.ljust(max_key_length), email.ljust(max_key_length),
+                                                           str(*phone)) + format.SEPARATOR)
 
-    @running.setter
-    def running(self, value):
-        self.__isRunning = value
 
-    def notes(self):  # deserialize notes
-        pass
-    
-    @property
-    def commands(self) -> dict:
-        return self.__commands
+# def singleton(class_):
+#     instances = {}
+#
+#     def getinstance(*args, **kwargs):
+#         if class_ not in instances:
+#             instances[class_] = class_(*args, **kwargs)
+#         return instances[class_]
+#
+#     return getinstance
+#
+#
+# @singleton
+# class Bot:
+#     def __init__(self):
+#         self.__isRunning = True
+#         self.__book = None
+#         self.__notes = None
+#         self.__commands = dict()
+#
+#     @property
+#     def running(self):
+#         return self.__isRunning
+#
+#     @running.setter
+#     def running(self, value):
+#         self.__isRunning = value
+#
+#     def notes(self):  # deserialize notes
+#         pass
+#
+#     @property
+#     def commands(self) -> dict:
+#         return self.__commands
+#
+#     @commands.setter
+#     def commands(self, commands: dict):
+#         self.__commands = commands
+#
+#     @property
+#     def note(self):
+#         pass
+#
+#     @note.setter
+#     def note(self, id):
+#         pass
 
-    @commands.setter
-    def commands(self, commands: dict):
-        self.__commands = commands
-
-    @property
-    def note(self):
-        pass
-
-    @note.setter
-    def note(self, id):
-        pass
 
 def bot_exit():
     print("Good bay")
@@ -187,6 +220,7 @@ def command_handler(command, input_string: str):
     user_command = Bot().commands.get(command)
     return user_command(*input_string)
 
+
 def bot_start():
     bot = Bot()
     COMMANDS = {
@@ -196,7 +230,7 @@ def bot_start():
         'exit': bot_exit,
         'change': change_phone,
         'phone': find_phone,
-        'show_all': None,
+        'show_all': show_all,
         'wrong command': None,
         'delete': del_user,
         'birthday': add_birthday,
@@ -207,16 +241,15 @@ def bot_start():
     bot.commands = COMMANDS
     while bot.running:
         try:
-            user_input = prompt(">>",completer=completer, bottom_toolbar=bottom_toolbar, style=style, rprompt=rprompt)
+            user_input = prompt(">>", completer=completer, bottom_toolbar=bottom_toolbar, style=style, rprompt=rprompt)
             # print(user_input)
             user_command, input_string = input_handler(str(user_input))
             txt_msg = command_handler(user_command, input_string)
-            if txt_msg != None:
-                print(Style.BRIGHT+Fore.LIGHTWHITE_EX + '\n' + txt_msg + Fore.RESET+Style.RESET_ALL)
-                
+            if txt_msg is not None:
+                print(Style.BRIGHT + Fore.LIGHTWHITE_EX + '\n' + txt_msg + Fore.RESET + Style.RESET_ALL)
+
         except ValueError as error_msg:
             print(Fore.LIGHTRED_EX + error_msg.args[0] + Fore.RESET)
-
 
 
 if __name__ == "__main__":
